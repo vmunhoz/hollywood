e9a8308ec0c584d3df05a653a2af600d3adf80b8
Vinicius Munhoz <vinicius@gryfo.com.br>
2019-07-30 16:18:38 -0300

    adicionando carros nas funcoes

--- hollywood.py
+++ hollywood.py
@@ -25,11 +25,23 @@ class Hollywood():
                     self.persons_paths.append(dirpath + '/' + f)
         self.persons_paths.sort()
 
+        self.cars_paths = []
+        for dirpath, dirnames, filenames in os.walk('cars'):
+            for f in filenames:
+                if f.endswith('.png') or f.endswith('.jpg'):
+                    self.cars_paths.append(dirpath + '/' + f)
+        self.cars_paths.sort()
+
     def _get_person_path(self, i=0):
         # return random.choice(self.persons_paths)
         while i > len(self.persons_paths) - 1:
             i -= len(self.persons_paths)
         return self.persons_paths[i]
+
+    def _get_car_path(self, i=0):
+        while i > len(self.cars_paths) - 1:
+            i -= len(self.cars_paths)
+        return self.cars_paths[i]
 
     def _fit_in_the_middle(self, frame, image):
         frame_y, frame_x, _ = frame.shape
@@ -75,58 +87,98 @@ class Hollywood():
         for _ in range(seconds * int(self.fps)):
             self.add_frame(white_frame)
 
-    def show_person(self, seconds, num_persons=1, rows=2, cols=5):
-        print('show_person: {} segundos, {} pessoas, em uma grid [{},{}]'.format(seconds, num_persons, rows, cols))
-        new_frame = np.zeros((self.height, self.width, 3), dtype='uint8') + 255
-        frame_y, frame_x, _ = new_frame.shape
-        last_offset_x = 0
-        next_offset_x = 0
-        last_offset_y = 0
+    def get_default_frame(self, rows, cols):
+        frame = np.zeros((self.height, self.width, 3), dtype='uint8') + 255
+        frame_y, frame_x, _ = frame.shape
 
         height = math.floor(frame_y / rows)
         width = math.floor(frame_x / cols)
 
-        for i in range(num_persons):
-            square = self._build_square(new_frame, rows, cols)
-            person_img = cv2.imread(self._get_person_path(i))
-            person_img = self._resize_to_fit_frame(new_frame, person_img, rows, cols)
-            square = self._fit_in_the_middle(square, person_img)
+        default_frame = {
+            'frame': frame,
+            'frame_y': frame_y,
+            'frame_x': frame_x,
+            'col_height': height,
+            'col_width': width,
+        }
 
-            person_y, person_x, _ = person_img.shape
+        return default_frame
+
+    def get_obj_path_func(self, obj_type):
+        if obj_type == 'person':
+            func = self._get_person_path
+        elif obj_type == 'car':
+            func = self._get_car_path
+        else:
+            print('obj_type \'{}\' not recognized. Using \'person\' instead...'.format(obj_type))
+        
+        return func
+
+    def get_show_frame(self, num_objs, rows, cols, obj_type='person'):
+        df = self.get_default_frame(rows, cols)
+
+        last_offset_x = 0
+        next_offset_x = 0
+        last_offset_y = 0
+
+        get_obj_path = self.get_obj_path_func(obj_type)
+
+        for i in range(num_objs):
+            square = self._build_square(df['frame'], rows, cols)
+            obj_img = cv2.imread(get_obj_path(i))
+            obj_img = self._resize_to_fit_frame(df['frame'], obj_img, rows, cols)
+            square = self._fit_in_the_middle(square, obj_img)
+
+            # obj_y, obj_x, _ = obj_img.shape
             offset_x = next_offset_x
             offset_y = last_offset_y
-            while (width + offset_x) > frame_x:
+            while (df['col_width'] + offset_x) > df['frame_x']:
                 offset_x = 0
-                offset_y = last_offset_y + height
+                offset_y = last_offset_y + df['col_height']
             
             last_offset_x = offset_x
-            # next_offset_x = last_offset_x + person_x
-            next_offset_x = last_offset_x + width
+            next_offset_x = last_offset_x + df['col_width']
             last_offset_y = offset_y
 
+            df['frame'][offset_y:df['col_height'] + offset_y,
+                      offset_x:df['col_width'] + offset_x] = square
+        
+        return df['frame']
 
-            # print('{} --- person x {}:{}, y {}:{}'.format(i, 0, person_x, 0, person_y))
-            # print('{} --- recorte x {}:{}, y {}:{}\n'.format(i, offset_x, person_x + offset_x, offset_y, person_y + offset_y))
-
-            new_frame[offset_y:height + offset_y,
-                      offset_x:width + offset_x] = square
-
-        # new_frame = cv2.add(new_frame, person_img)
+    def show_car(self, seconds, num_cars=1, rows=2, cols=3):
+        print('show_car: {} segundos, {} carros, em uma grid [{},{}]'.format(seconds, num_cars, rows, cols))
+        frame = self.get_show_frame(num_cars, rows, cols, obj_type='car')
         for _ in range(seconds * int(self.fps)):
-            self.add_frame(new_frame)
+            self.add_frame(frame)
+
+    def show_person(self, seconds, num_persons=1, rows=2, cols=5):
+        print('show_person: {} segundos, {} pessoas, em uma grid [{},{}]'.format(seconds, num_persons, rows, cols))
+        frame = self.get_show_frame(num_persons, rows, cols, obj_type='person')
+        for _ in range(seconds * int(self.fps)):
+            self.add_frame(frame)
 
     def _walk_one(self, current_position, step):
         return (current_position[0] + step[0], current_position[1] + step[1])
 
-    def slide_person(self, move_seconds, hold_seconds=0, slide_direction='down', rows=2, cols=5):
+    def slide_car(self, move_seconds, hold_seconds=0, slide_direction='down', rows=2, cols=3, index=0):
+        self.slide_obj(move_seconds, hold_seconds=hold_seconds, slide_direction=slide_direction, rows=rows, cols=cols, obj_type='car', index=index)
+
+    def slide_person(self, move_seconds, hold_seconds=0, slide_direction='down', rows=2, cols=5, index=0):
+        self.slide_obj(move_seconds, hold_seconds=hold_seconds, slide_direction=slide_direction, rows=rows, cols=cols, obj_type='person', index=index)
+
+    def slide_obj(self, move_seconds, hold_seconds=0, slide_direction='down', rows=2, cols=5, obj_type='person', index=0):
         """ possible border_from = up, right, down, left """
+        print('slide_{}: {} segundos, slide_direction {}, em uma grid [{},{}]'.format(obj_type, move_seconds, slide_direction, rows, cols))
+
         frame = np.zeros((self.height, self.width, 3), dtype='uint8') + 255
         frame_y, frame_x, _ = frame.shape
 
+        get_obj_path = self.get_obj_path_func(obj_type)
+
         square = self._build_square(frame, rows, cols)
-        person_img = cv2.imread(self._get_person_path())
-        person_img = self._resize_to_fit_frame(frame, person_img, rows, cols)
-        square = self._fit_in_the_middle(square, person_img)
+        obj_img = cv2.imread(get_obj_path(index))
+        obj_img = self._resize_to_fit_frame(frame, obj_img, rows, cols)
+        square = self._fit_in_the_middle(square, obj_img)
 
         square_y, square_x, _ = square.shape
         total_frames = move_seconds * int(self.fps)
@@ -156,23 +208,13 @@ class Hollywood():
 
         for i in range(total_frames):
             frame = np.zeros((self.height, self.width, 3), dtype='uint8') + 255
-            # print('--------------------------')
-            # print(pos[0], pos[0] + square_y, (pos[0] + square_y) - pos[0] )
-            # print(frame[pos[0]:square_y + pos[0], pos[1]:square_x + pos[1]].shape)
-            # print(square.shape)
 
             overflow_y = max(0, (square_y + pos[0]) - frame_y)
             overflow_x = max(0, (square_x + pos[1]) - frame_x)
 
-            # print(overflow_y, overflow_x)
-            # print(square_y - overflow_y, square_x - overflow_x)
-            # print('--------------------------')
-
             if square_y - overflow_y < 0 or square_x - overflow_x < 0:
-                print(pos)
-                print('D=')
+                # print(pos, 'removendo pedaco da imagem')
                 continue
-
 
             init_frame_y = pos[0]
             init_square_y = 0
@@ -193,23 +235,11 @@ class Hollywood():
 
             self.add_frame(frame)
             pos = self._walk_one(pos, step)
-        print(final_position)
+        # print(final_position)
 
 
     def close_video(self):
         self.video.release()
         print(self.video)
-
-
-
-# img1 = cv2.imread('1.jpg')
-# height, width, layers = img1.shape
-
-# video.write(img1)
-# video.write(img2)
-# video.write(img3)
-
-# cv2.destroyAllWindows()
-# video.release()
 
 
--- sample.py
+++ sample.py
@@ -3,19 +3,33 @@ from hollywood import Hollywood
 h = Hollywood()
 
 
-h.wait(1)  # espera 10 segundos
+h.wait(1)  # espera 1 segundo
 
+h.show_car(2, num_cars=1, rows=1, cols=1)
+h.show_car(2, num_cars=5)
 
-# h.show_person(3, num_persons=3, rows=1, cols=3)
-# h.show_person(10, num_persons=10)
+h.wait(1)
+
+h.show_person(2, num_persons=1, rows=1, cols=1)
+h.show_person(2, num_persons=3, rows=1, cols=3)
+h.show_person(2, num_persons=5)
+
+h.wait(1)
+
+h.slide_car(3, slide_direction="down", rows=1, cols=1)
+h.slide_car(3, slide_direction="up", rows=1, cols=1, index=1)
+h.slide_car(3, slide_direction="right", rows=1, cols=1, index=2)
+h.slide_car(3, slide_direction="left", rows=1, cols=1, index=3)
+
+h.wait(1)
 
 h.slide_person(3, slide_direction="down", rows=1, cols=1)
-h.slide_person(3, slide_direction="up", rows=1, cols=1)
-h.slide_person(5, slide_direction="right", rows=1, cols=1)
-h.slide_person(5, slide_direction="left", rows=1, cols=1)
+h.slide_person(3, slide_direction="up", rows=1, cols=1, index=1)
+h.slide_person(3, slide_direction="right", rows=1, cols=1, index=2)
+h.slide_person(3, slide_direction="left", rows=1, cols=1, index=3)
 
 
-h.wait(1)  # espera 10 segundos
+h.wait(1)
 
 
 h.close_video()  # finaliza o video
